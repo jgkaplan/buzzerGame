63ac29a62c2540f4eb0e92c85773b6348282929e
jgkaplan <joshuagraykaplan@gmail.com>
2016-04-16 01:33:54 -0500

    Working single server

--- index.js
+++ index.js
@@ -7,32 +7,53 @@ app.get('/', function(req, res){
     res.sendFile(__dirname+'/index.html');
 });
 
+app.get('/host', function(req, res){
+    res.sendFile(__dirname+'/host.html');
+});
+
+app.get('/player', function(req, res){
+    res.sendFile(__dirname+'/player.html');
+})
+
 app.use(express.static(path.join(__dirname,"/public")));
 app.use('/bower_components', express.static(path.join(__dirname,"/bower_components")));
 app.use('/node_modules/socket.io-client', express.static(path.join(__dirname,'/node_modules','/socket.io-client')))
 
 var io = require('socket.io').listen(app.listen(port));
-var ready = true;
-var Users = [];
+var host = io.of('/host');
+var player = io.of('/player');
 
-io.on('connection', function(socket){
-    socket.on('playerJoin', function(player){
-        io.emit('newPlayer', player);
+var ready = true;
+var currentlyGuessing;
+
+player.on('connection', function(socket){
+    socket.on('playerJoin', function(name){
+        host.emit('playerJoin', socket.id, name);
     });
-    socket.on('result', function(emit){
-        io.emit(emit);
+    socket.on('disconnect', function(){
+        host.emit('playerLeave', socket.id);
     });
-    socket.on('answer', function(playerName){
+    socket.on('answer', function(callback){
         if(ready){
-            io.emit('playerGuess', playerName);
+            host.emit('playerGuess', socket.id);
             ready = false;
+            currentlyGuessing = socket.id;
+            callback(true);
+        }else{
+            callback(false);
         }
+    });
+});
+
+host.on('connection', function(socket){
+    socket.on('success', function(){
+        player.to(currentlyGuessing).emit('success');
+    });
+    socket.on('failure', function(){
+        player.to(currentlyGuessing).emit('failure');
     });
     socket.on('continue',function(){
         ready = true;
-        socket.broadcast.emit('continue');
+        player.emit('continue');
     });
-    socket.on('isReady', function(){
-        return ready;
-    })
 });
